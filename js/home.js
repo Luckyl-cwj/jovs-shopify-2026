3d1960f6234f79de420d248f13d5bb0e32c74853
luckyl.cwj@gmail.com <luckyl.c>
2026-03-15 16:36:45 +0800

    chore: update project files

---
+++ js/home.js
@@ -0,0 +1,31 @@
+
+var swiperProhibitSelectThumb = new Swiper(".swiper-prohibit-select-thumb", {
+  spaceBetween: 30,
+
+  effect: "fade",
+});
+var swiperProhibitSelect = new Swiper(".swiper-prohibit-select", {
+  spaceBetween: 30,
+  loop: 'true',
+  navigation: {
+    nextEl: ".swiper-prohibit-select-navigation .swiper-button-next",
+    prevEl: ".swiper-prohibit-select-navigation .swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiperProhibitSelectThumb,
+  },
+  on: {
+    slideChange: function () {
+      swiperProhibitSelect.swiperIndex
+      console.log(1);
+
+    }
+
+  }
+});
+document.querySelector('.swiper-prohibit-select-progress .btn-swiper-prev').addEventListener('click', function () {
+  swiperProhibitSelect.slidePrev()
+})
+document.querySelector('.swiper-prohibit-select-progress .btn-swiper-next').addEventListener('click', function () {
+  swiperProhibitSelect.slideNext()
+})
